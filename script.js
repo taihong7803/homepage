0ecfcd3d5daa05dee632787f77345a6020ced00b
taihong <39943500+taihong7803@users.noreply.github.com>
2024-07-01 20:25:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,39 @@ document.addEventListener("DOMContentLoaded", function() {
     menuIcon.addEventListener('click', function() {
         navList.classList.toggle('nav-active');
     });
+
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        const slides = document.getElementsByClassName("slide");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 3000); // 3秒ごとに画像を切り替え
+    }
+
+    document.querySelector('.prev').addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    document.querySelector('.next').addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    function plusSlides(n) {
+        slideIndex += n;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        } else if (slideIndex < 1) {
+            slideIndex = slides.length;
+        }
+        showSlides();
+    }
 });
