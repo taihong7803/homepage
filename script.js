a1d62429a9b70f4cbc11c2b9b56dc9fbb73de6d2
taihong <39943500+taihong7803@users.noreply.github.com>
2024-07-01 20:31:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -33,11 +33,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function plusSlides(n) {
         slideIndex += n;
+        const slides = document.getElementsByClassName("slide");
         if (slideIndex > slides.length) {
             slideIndex = 1;
         } else if (slideIndex < 1) {
             slideIndex = slides.length;
         }
-        showSlides();
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "block";
     }
 });
